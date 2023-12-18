e6eb580ba13f1aa2e7a980fd33de95cef8f9b880
ilmir22 <152898248+ilmir22@users.noreply.github.com>
2023-12-18 20:25:26 +0300

    Add files via upload
    Задание 1
    Пользователь вводит с клавиатуры два числа. Нужно 
    показать все числа в указанном диапазоне.

---
+++ Practica1.py
@@ -0,0 +1,10 @@
+a = int(input("Enter the start number: "))
+b = int(input("Enter the last number: "))
+for i in range (a, b + 1):
+    print(i)
+
+print ("")
+
+while a < b + 1:
+    print(a)
+    a +=1
