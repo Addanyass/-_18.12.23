9fc465ee2c31d4401082e272a3a158f11ac9fee7
ilmir22 <152898248+ilmir22@users.noreply.github.com>
2023-12-18 20:26:46 +0300

    Add files via upload
    Задание 2
    Пользователь вводит с клавиатуры два числа. Нужно 
    показать все нечетные числа в указанном диапазоне.

---
+++ Practica2.py
@@ -0,0 +1,5 @@
+a = int(input("Enter the first number"))
+b = int(input("Enter the  second number"))
+for i in range (a, b + 1):
+    if (i % 2 != 0): #Если остаток деления на 2 не равен 0 то операция выполняется 
+        print(i)
